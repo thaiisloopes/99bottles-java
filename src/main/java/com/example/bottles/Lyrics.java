e88988f07491e5f954499f38ebd22ec31a33ee95
Thais Nunes <tlnunes@thoughtworks.com>
2020-03-31 17:56:46 -0300

    Fix none bottles test

--- src/main/java/com/example/bottles/Lyrics.java
+++ src/main/java/com/example/bottles/Lyrics.java
@@ -10,6 +10,9 @@ public class Lyrics {
         } else if (bottle == 1) {
             return bottle + " bottle of beer on the wall, " + bottle + " bottle of beer.\n" +
                     "Take one down and pass it around, no more bottles of beer on the wall.";
+        } else if (bottle == 0) {
+            return "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                    "Go to the store and buy some more, 99 bottles of beer on the wall.";
         } else {
             return bottle+ " bottles of beer on the wall, "+ bottle + " bottles of beer.\n"+
                     "Take one down and pass it around, "+ bottleMinusOne +" bottles of beer on the wall.";
