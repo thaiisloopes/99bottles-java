1ba9073b8ad50d8df510fb31376e927034b07dff
Thais Nunes <tlnunes@thoughtworks.com>
2020-04-01 11:27:58 -0300

    Refactor main method

--- src/main/java/com/example/bottles/Lyrics.java
+++ src/main/java/com/example/bottles/Lyrics.java
@@ -9,18 +9,28 @@ public class Lyrics {
     private String buildVerse(int bottle) {
         int bottleMinusOne = bottle - 1;
 
+        if (bottle > 2) {
+            return buildStandardVerse(bottle, bottleMinusOne);
+        } else {
+            return buildSpecificVerse(bottle, bottleMinusOne);
+        }
+    }
+
+    private String buildStandardVerse(int bottle, int bottleMinusOne) {
+        return bottle+ " bottles of beer on the wall, "+ bottle + " bottles of beer.\n"+
+                "Take one down and pass it around, "+ bottleMinusOne +" bottles of beer on the wall.";
+    }
+
+    private String buildSpecificVerse(int bottle, int bottleMinusOne) {
         if (bottle == 2) {
             return bottle+ " bottles of beer on the wall, "+ bottle + " bottles of beer.\n"+
                     "Take one down and pass it around, "+ bottleMinusOne +" bottle of beer on the wall.";
         } else if (bottle == 1) {
             return bottle + " bottle of beer on the wall, " + bottle + " bottle of beer.\n" +
                     "Take one down and pass it around, no more bottles of beer on the wall.";
-        } else if (bottle == 0) {
+        } else {
             return "No more bottles of beer on the wall, no more bottles of beer.\n" +
                     "Go to the store and buy some more, 99 bottles of beer on the wall.";
-        } else {
-            return bottle+ " bottles of beer on the wall, "+ bottle + " bottles of beer.\n"+
-                    "Take one down and pass it around, "+ bottleMinusOne +" bottles of beer on the wall.";
         }
     }
 }
